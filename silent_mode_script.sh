0dfa76453914bf6bf8e1a930abf9480dd3e9d406
Jared J <26422790+ipaqmaster@users.noreply.github.com>
2026-05-25 23:18:49 +1000

    Dramatically reduce ipmitool sdr sweep time by caching the Sensor Data Records (SDR)
    Down from 0m7.054s to 0m0.737s with the cache file.

--- silent_mode_script.sh
+++ silent_mode_script.sh
@@ -2,13 +2,44 @@
 
 [ $UID -ne 0 ] && command -v sudo >/dev/null 2>&1 && SUDO=sudo # Use sudo if not root
 
-ipmi_output=$(ipmitool sdr 2>/dev/null)
-fan_zones=($(echo "$ipmi_output" | grep 'Fan [0-9][A-Z] Tach' | awk -F'|' '{print $1}' | awk '{print $2}'))
+# Use a cache file so ipmitool sdr returns quicker than being run blind each loop
+ipmitool_sdr_cache_file_path=/tmp/ipmitool_sdr_cache.bin
+
+
+function generate_ipmitool_sdr_cache {
+    echo "Generating ipmitool sdr dump file..." >&2
+    ipmitool sdr dump "${ipmitool_sdr_cache_file_path:-ipmitool.sdr.cache.bin}" 2>/dev/null
+}
+
+function get_ipmitool_sdr_output {
+
+  # Generate an ipmitool sdr cache file
+  # This results in more responsive 'ipmitool sdr' passes.
+  if ! [ -f "${ipmitool_sdr_cache_file_path:-ipmitool.sdr.cache.bin}" ]
+  then
+    generate_ipmitool_sdr_cache
+  else
+    # Update the cache file if its older than an hour in age
+    # (Purposefully added to avoid one system using another's cache file if errornously copied over to avoid running wrong)
+    cache_file_age=$(($(date +%s) - $(stat -c %Y "${ipmitool_sdr_cache_file_path:-ipmitool.sdr.cache.bin}")))
+    if [ ${cache_file_age} -gt 3600 ]
+    then
+      generate_ipmitool_sdr_cache
+    fi
+  fi
+
+  # Generate the output with the help of the cache file to speed the pass up.
+  ipmitool sdr -S "${ipmitool_sdr_cache_file_path:-ipmitool.sdr.cache.bin}" 2>/dev/null
+}
+
+
+ipmi_output="$(get_ipmitool_sdr_output)"
+fan_zones=($(echo "${ipmi_output}" | grep 'Fan [0-9][A-Z] Tach' | awk -F'|' '{print $1}' | awk '{print $2}'))
 
 while true; do
-  ipmi_output=$(ipmitool sdr 2>/dev/null)
-  cpu_output=$(echo "$ipmi_output" | grep -i "CPU .* Temp")
-  cpu_temps=($(echo "$ipmi_output" | grep 'CPU [0-9] Temp' | awk -F'|' '{print $2}' | awk '{print $1}'))
+  ipmi_output="$(get_ipmitool_sdr_output)"
+  cpu_output=$(echo "${ipmi_output}" | grep -i "CPU .* Temp")
+  cpu_temps=($(echo "${ipmi_output}" | grep 'CPU [0-9] Temp' | awk -F'|' '{print $2}' | awk '{print $1}'))
 
   echo "Currrent CPU temp: ${cpu_temps[@]}"
 
